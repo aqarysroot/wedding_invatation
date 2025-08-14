23c982bcc0e2d4702f398ae0416eecf950b63efe
aqarysroot <61238089+aqarysroot@users.noreply.github.com>
2025-08-14 15:48:53 +0500

    Revert "minor improvements, added routing"

--- front_app/src/main.tsx
+++ front_app/src/main.tsx
@@ -1,12 +1,11 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "@/App";
-import { BrowserRouter } from "react-router-dom";
-
 import "@/assets/styles/globals.scss";
 import "@/i18n"
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 );
